8592ac8d0867009fabcaaf6259f7b15b922139f0
Kitsakorn Petchyen <kitsakornwin@gmail.com>
2026-08-05 21:04:17 +0700

    Add: k-closest-points-to-origin - submission-11

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-11.py
@@ -0,0 +1,30 @@
+class Solution:
+    def kClosest(
+        self, points: List[List[int]], k: int, s: int = 0, e: Optional[int] = None
+    ) -> List[List[int]]:
+        if e == None:
+            e = len(points) - 1
+
+        if e - s + 1 <= 1:
+            return points[:k]
+
+        pivot = points[e]
+        left = s
+
+        for i in range(s, e):
+            dist_i = points[i][0] ** 2 + points[i][1] ** 2
+            dist_pivot = pivot[0] ** 2 + pivot[1] ** 2
+
+            if dist_i < dist_pivot:
+                points[left], points[i] = points[i], points[left]
+                left += 1
+
+        points[e] = points[left]
+        points[left] = pivot
+
+        if left == k:
+            return points[:k]
+        elif left < k:
+            return self.kClosest(points, k, left + 1, e)
+        else:
+            return self.kClosest(points, k, s, left - 1)
