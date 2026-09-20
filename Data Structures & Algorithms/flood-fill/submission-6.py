5c57f0abc5fb142668c6f31c8787441fa19c2631
Kitsakorn Petchyen <kitsakornwin@gmail.com>
2026-09-20 10:34:27 +0700

    Add: flood-fill - submission-6

---
+++ Data Structures & Algorithms/flood-fill/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # To solve this problem, I will implement DFS approach
+        # 1. I will declare the original color
+        original_color = image[sr][sc]
+
+        # 2. I will make an early return, if the original color is the same color with target color
+        if original_color == color:
+            return image
+
+        # 3. I will declare a size or row and column
+        ROWS, COLS = len(image), len(image[0])
+
+        # 4. I will declare a helper DFS function to fill in the colors recursively
+        def dfs(r, c):
+            # 5. I will declare a base case to return when current cell can't be filled with the target color
+            if min(r, c) < 0 or r == ROWS or c == COLS or image[r][c] != original_color:
+                return
+
+            # 6. Fill the new color into the cell
+            image[r][c] = color
+            # 7. I will call dfs() to traverse to 4 directions for each cell adjacent nodes
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        dfs(sr, sc)
+        return image
+
+        # Time: O(n*m) Even though we traverse in 4 directions for each cell but we have a condition to not traverse back to the cell that we already filled the new color
+        # Space: O(n*m) Even though we don't use extra auxiliary structures, space is required for the implicit recursion call stack.
